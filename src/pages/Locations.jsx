48496df40035e282cc10076055411bcbf0b460ac
VCarames <109115310+VCarames@users.noreply.github.com>
2023-08-12 10:35:27 -0700

    Not List Section Created [JSX]

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -22,6 +22,22 @@ function Locations() {
           <Map />
         </section>
 
+        <section className="not-listed-section">
+          <div className="not-listed-section__content">
+            <h2 className="not-listed-section__heading">
+              Your City Not Listed?
+            </h2>
+            <p className="not-listed-section__text">
+              If you’d like to see Scoot in your hometown, be sure to let us
+              know. We track requests and plan launches based on demand. Feel
+              free to message us by clicking the link or messaging us on social.
+            </p>
+          </div>
+          <a href="/" className="not-listed-section__btn button">
+            Message Us
+          </a>
+        </section>
+
         <CTA />
       </main>
       <Footer />
