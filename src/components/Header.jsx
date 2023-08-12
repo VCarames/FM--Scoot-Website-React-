1fc9c7a378f6ce5ab5f74f25003140504b847eb5
VCarames <109115310+VCarames@users.noreply.github.com>
2023-08-12 00:13:43 -0700

    Header + Nav Function Added [JSX]
    "Scroll" class is added to Header which add a "box-shadow" when the Header is scrolled over.
    
    When users click on the nav button, the nav menu will appear.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
+import React, { useState, useEffect } from "react";
 import Logo from "/logo.svg";
 import NavBar from "./NavBar";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setIsScrolled(window.scrollY > 0);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header className="header">
+      <header className={`header ${isScrolled ? "scrolled" : ""}`}>
         <div className="header__content">
           <Link to="/" className="header__logo" aria-label="Home - Scoot">
             <img src={Logo} alt="" />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,43 +1,99 @@
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const navMenuRef = useRef();
+  const navToggleRef = useRef();
+
+  useEffect(() => {
+    function handleEscapeKey(event) {
+      if (event.key === "Escape") {
+        closeNav();
+      }
+    }
+
+    function handleClickOutside(event) {
+      if (
+        navMenuRef.current &&
+        !navMenuRef.current.contains(event.target) &&
+        event.target !== navToggleRef.current
+      ) {
+        closeNav();
+      }
+    }
+
+    document.addEventListener("keydown", handleEscapeKey);
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const toggleNav = () => {
+    setIsNavExpanded((prevExpanded) => !prevExpanded);
+    if (!isNavExpanded) {
+      document.body.classList.add("disable-scroll");
+    } else {
+      document.body.classList.remove("disable-scroll");
+    }
+  };
+
+  const closeNav = () => {
+    setIsNavExpanded(false);
+    document.body.classList.remove("disable-scroll");
+  };
+
+  const handleFocusOut = (event) => {
+    if (!navMenuRef.current.contains(event.relatedTarget)) {
+      closeNav();
+    }
+  };
+
   return (
-    <>
-      <nav className="header__nav" aria-label="main">
-        <button
-          className="header__nav-toggle"
-          aria-expanded="false"
-          aria-controls="header__nav-menu-list"
-          aria-label="menu"
-        >
-          <span className="header__nav-toggle-line" />
-          <span className="header__nav-toggle-line" />
-          <span className="header__nav-toggle-line" />
-        </button>
-        <div className="header__nav-menu">
-          <ul className="header__nav-menu-list" role="list">
-            <li className="header__nav-menu-item" role="listitem">
-              <Link to="/about" className="header__nav-menu-link">
-                About
-              </Link>
-            </li>
-            <li className="header__nav-menu-item" role="listitem">
-              <Link to="/locations" className="header__nav-menu-link">
-                Location
-              </Link>
-            </li>
-            <li className="header__nav-menu-item" role="listitem">
-              <Link to="/careers" className="header__nav-menu-link">
-                Careers
-              </Link>
-            </li>
-          </ul>
-          <a href="" className="header__nav-menu-btn button">
-            Get Scootin
-          </a>
-        </div>
-      </nav>
-    </>
+    <nav className="header__nav" aria-label="main">
+      <button
+        className="header__nav-toggle"
+        ref={navToggleRef}
+        aria-expanded={isNavExpanded}
+        aria-controls="header__nav-menu-list"
+        aria-label="menu"
+        onClick={toggleNav}
+      >
+        <span className="header__nav-toggle-line" />
+        <span className="header__nav-toggle-line" />
+        <span className="header__nav-toggle-line" />
+      </button>
+
+      <div
+        className={`header__nav-menu ${isNavExpanded ? "expanded" : ""}`}
+        ref={navMenuRef}
+        onBlur={handleFocusOut}
+      >
+        <ul className="header__nav-menu-list" role="list">
+          <li className="header__nav-menu-item" role="listitem">
+            <Link to="/about" className="header__nav-menu-link">
+              About
+            </Link>
+          </li>
+          <li className="header__nav-menu-item" role="listitem">
+            <Link to="/locations" className="header__nav-menu-link">
+              Locations
+            </Link>
+          </li>
+          <li className="header__nav-menu-item" role="listitem">
+            <Link to="/careers" className="header__nav-menu-link">
+              Careers
+            </Link>
+          </li>
+        </ul>
+        <a href="" className="header__nav-menu-btn button">
+          Get Scootin
+        </a>
+      </div>
+    </nav>
   );
 }
 
